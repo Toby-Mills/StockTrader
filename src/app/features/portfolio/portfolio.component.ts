eae3a11cdc676e2f064f9fbf53292f64dde3b99c
Toby Mills <toby.mills@spatialdimension.com>
2026-06-07 16:38:33 +0200

    feat: update styles for section titles and add icons across components

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     standalone: true,
     selector: 'app-portfolio',
-    imports: [MatCardModule],
+    imports: [MatCardModule, MatIconModule],
     templateUrl: './portfolio.component.html',
     styleUrl: './portfolio.component.scss'
 })
