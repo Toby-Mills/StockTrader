d3fdaef276bc4878fedf6f7f0ac2b1e8eb1f4af1
Toby Mills <toby.mills@spatialdimension.com>
2026-06-08 16:35:59 +0200

    feat: add styles and tests for account history and snapshot dialog components

--- src/app/features/analytics/account-history/account-history.component.spec.ts
+++ src/app/features/analytics/account-history/account-history.component.spec.ts
@@ -1,19 +1,60 @@
+/// <reference types="jasmine" />
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AccountHistoryComponent } from './account-history.component';
+import { MatDialog } from '@angular/material/dialog';
+import { AccountHistoryService } from '../../../core/services/account-history.service';
+import { PortfolioService } from '../../../core/services/portfolio.service';
+import { TransactionService } from '../../../core/services/transaction.service';
+import { DividendService } from '../../../core/services/dividend.service';
+import { CashEventService } from '../../../core/services/cash-event.service';
+import { of } from 'rxjs';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AccountHistoryComponent', () => {
   let component: AccountHistoryComponent;
   let fixture: ComponentFixture<AccountHistoryComponent>;
 
+  const mockHistoryService = {
+    getSnapshots: jasmine.createSpy('getSnapshots').and.returnValue(of([]))
+  };
+  const mockPortfolioService = {
+    getPortfolioSnapshot: jasmine.createSpy('getPortfolioSnapshot').and.returnValue(of({ holdings: [] }))
+  };
+  const mockTransactionService = {
+    getTransactions: jasmine.createSpy('getTransactions').and.returnValue(of([]))
+  };
+  const mockDividendService = {
+    getDividends: jasmine.createSpy('getDividends').and.returnValue(of([]))
+  };
+  const mockCashEventService = {
+    getCashEvents: jasmine.createSpy('getCashEvents').and.returnValue(of([]))
+  };
+  const mockDialog = {
+    open: jasmine.createSpy('open')
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AccountHistoryComponent]
+      imports: [
+        AccountHistoryComponent,
+        NoopAnimationsModule
+      ],
+      providers: [
+        { provide: AccountHistoryService, useValue: mockHistoryService },
+        { provide: PortfolioService, useValue: mockPortfolioService },
+        { provide: TransactionService, useValue: mockTransactionService },
+        { provide: DividendService, useValue: mockDividendService },
+        { provide: CashEventService, useValue: mockCashEventService },
+        { provide: MatDialog, useValue: mockDialog }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(AccountHistoryComponent);
     component = fixture.componentInstance;
+    // Set required input
+    fixture.componentRef.setInput('accountId', 'test-acc');
+    fixture.componentRef.setInput('currency', 'GBP');
     fixture.detectChanges();
   });
 
@@ -21,3 +62,4 @@ describe('AccountHistoryComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
--- src/app/features/analytics/account-snapshot-dialog/account-snapshot-dialog.component.spec.ts
+++ src/app/features/analytics/account-snapshot-dialog/account-snapshot-dialog.component.spec.ts
@@ -1,14 +1,44 @@
+/// <reference types="jasmine" />
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AccountSnapshotDialogComponent } from './account-snapshot-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PortfolioService } from '../../../core/services/portfolio.service';
+import { TransactionService } from '../../../core/services/transaction.service';
+import { DividendService } from '../../../core/services/dividend.service';
+import { CashEventService } from '../../../core/services/cash-event.service';
+import { of } from 'rxjs';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 describe('AccountSnapshotDialogComponent', () => {
   let component: AccountSnapshotDialogComponent;
   let fixture: ComponentFixture<AccountSnapshotDialogComponent>;
 
+  const mockPortfolioService = {
+    getPortfolioSnapshot: jasmine.createSpy('getPortfolioSnapshot').and.returnValue(of({ holdings: [] }))
+  };
+  const mockTransactionService = {};
+  const mockDividendService = {};
+  const mockCashEventService = {};
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AccountSnapshotDialogComponent]
+      imports: [
+        AccountSnapshotDialogComponent,
+        NoopAnimationsModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: { accountId: 'test-acc', currency: 'GBP' } },
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: PortfolioService, useValue: mockPortfolioService },
+        { provide: TransactionService, useValue: mockTransactionService },
+        { provide: DividendService, useValue: mockDividendService },
+        { provide: CashEventService, useValue: mockCashEventService },
+        provideNativeDateAdapter()
+      ]
     })
     .compileComponents();
 
@@ -21,3 +51,4 @@ describe('AccountSnapshotDialogComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
